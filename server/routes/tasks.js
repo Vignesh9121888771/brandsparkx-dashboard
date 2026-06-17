7125471437ec01549e38f9669608f5bbbd6e6c28
Vignesh9121888771 <b.vignesh021a@gmail.com>
2026-06-17 14:39:54 +0530

    fix: remove progress routes to unblock server for review

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -3,13 +3,9 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const ctrl = require('../controllers/tasksController');
 
-router.get('/progress/pending', auth, ctrl.getPendingUpdates);
-router.put('/progress/:update_id/review', auth, ctrl.reviewProgressUpdate);
 router.get('/', auth, ctrl.getAllTasks);
 router.post('/', auth, ctrl.createTask);
 router.put('/:id', auth, ctrl.updateTask);
 router.delete('/:id', auth, ctrl.deleteTask);
-router.post('/:id/progress', auth, ctrl.submitProgressUpdate);
-router.get('/:id/progress/history', auth, ctrl.getProgressHistory);
 
 module.exports = router;
